b9c794626fcd3edf5dabd8a2e6050dc1e218d29e
SamuelWiniger <winiger.samuel@gmail.com>
2016-08-11 16:28:49 +0200

    Changed name
    Adjusated  store and index
    download no longer needed

--- store.php
+++ store.php
@@ -4,22 +4,21 @@
 
 // write the file to disk
 $subjectID = $_POST['subjectID'];
-$folder = $_POST['folder'];
+$data_dir = $_POST['folder'];
 $csvData = $_POST['csvStrings'];
 $jsonData = $_POST['dataAsJSON'];
-$openshift_data_dir = $_ENV["OPENSHIFT_DATA_DIR"];
 $timestr = date('Y-m-d_H:i:s');
 
 //at first, write a backup copy of the json file into backup folder
-if(!file_exists($openshift_data_dir.'json/')){ mkdir($openshift_data_dir.'json/',0777,true);}
+if(!file_exists($data_dir.'json/')){ mkdir($data_dir.'json/',0777,true);}
 for ($i = 0; $i < sizeof($jsonData); $i++) {
-      file_put_contents($openshift_data_dir.'json/'.$timestr.'_'.$subjectID.'.txt', $jsonData);
+      file_put_contents($data_dir.'json/'.$timestr.'_'.$subjectID.'.txt', $jsonData);
 };
 
 
 // then put csvStrings in experiment folder.
-if(!file_exists($openshift_data_dir.'csv/')){ mkdir($openshift_data_dir.'csv/',0777,true);}
+if(!file_exists($data_dir.'csv/')){ mkdir($data_dir.'csv/',0777,true);}
 for ($i = 0; $i < sizeof($csvData); $i++) {
-      file_put_contents($openshift_data_dir.'csv/'.$timestr.'_'.$subjectID.'.csv', $csvData);
+      file_put_contents($data_dir.'csv/'.$timestr.'_'.$subjectID.'.csv', $csvData);
 }
 ?>
